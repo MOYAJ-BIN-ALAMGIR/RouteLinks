d75ba7d23b34033f1d662d815a4a23482f70aa97
Moyaj Bin Alamgir <130137627+MOYAJ-BIN-ALAMGIR@users.noreply.github.com>
2025-08-23 14:54:02 +0600

    Passing Value to Query Sting

--- RouteLinks/Controllers/ProductController.cs
+++ RouteLinks/Controllers/ProductController.cs
@@ -1,12 +1,13 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Xml.Linq;
 
 namespace RouteLinks.Controllers
 {
     public class ProductController : Controller
     {
-        public string Index(int id)
+        public string Index(int id, string name)
         {
-            return "Id Value is: " + id;
+            return "Id Value is: " + id + " Name value is: " + name;
         }
     }
 }
