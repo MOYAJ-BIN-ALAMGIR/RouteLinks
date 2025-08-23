0ac7bdf96b1e4162650eaeb6dbf6b523742f4d08
Moyaj Bin Alamgir <130137627+MOYAJ-BIN-ALAMGIR@users.noreply.github.com>
2025-08-23 14:43:20 +0600

    asp-route-{value}

---
+++ RouteLinks/Controllers/ProductController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RouteLinks.Controllers
+{
+    public class ProductController : Controller
+    {
+        public string Index(int id)
+        {
+            return "Id Value is: " + id;
+        }
+    }
+}
