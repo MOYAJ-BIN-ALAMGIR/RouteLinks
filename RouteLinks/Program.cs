916f6b8681151c80b8d2098b7ad886926d7cd606
Moyaj Bin Alamgir <130137627+MOYAJ-BIN-ALAMGIR@users.noreply.github.com>
2025-08-24 18:16:46 +0600

    Append Trailing slash (/) to the URLs

--- RouteLinks/Program.cs
+++ RouteLinks/Program.cs
@@ -5,6 +5,11 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+builder.Services.Configure<RouteOptions>(options =>
+{
+    options.AppendTrailingSlash = true;
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
